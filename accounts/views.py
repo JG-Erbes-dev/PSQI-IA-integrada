1be0921802506361b3e45f0461f037b2f3973735
José Guilherme Lopes Erbes <jg.dev.new@gmail.com>
2024-09-22 22:10:40 -0300

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
+
 def register_view(request):
     if request.method == "POST":
         user_form = CustomUserCreationForm(request.POST)
